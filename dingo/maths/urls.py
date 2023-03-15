84fbdf73eb1fe958268dd99eae62828671e6afa4
rafaltaras <paulo.dorosz@gmail.com>
2023-03-15 15:12:27 +0100

    forms

--- dingo/maths/urls.py
+++ dingo/maths/urls.py
@@ -1,11 +1,15 @@
 # maths/urls.py
 from django.urls import path
-from .views import math, add, sub, mul, div
+from .views import math, add, sub, mul, div, maths_list, math_details, results_list
 
+app_name="maths"
 urlpatterns = [
    path('', math),
-   path('add/<a>/<b>', add),
-   path('sub/<a>/<b>', sub),
-   path('mul/<a>/<b>', mul),
-   path('div/<a>/<b>', div)
+   path('add/<int:a>/<b>', add),
+   path('sub/<int:a>/<b>', sub),
+   path('mul/<int:a>/<b>', mul),
+   path('div/<int:a>/<b>', div),
+   path('histories/', maths_list, name="list"),
+   path('histories/<int:id>', math_details, name="details"),
+   path('results/', results_list, name="results"),
 ]
--- dingo/maths/views.py
+++ dingo/maths/views.py
@@ -1,50 +1,106 @@
-# maths/views.py
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import render
 from django.contrib import messages
+from .forms import ResultForm
+
+from maths.models import Math, Result
 
 def math(request):
    return HttpResponse("Tu będzie matma")
 
 def add(request, a, b):
-	a, b = int(a), int(b)
-	wynik = a+b
-	c = {"a":a, "b":b, "operacja": "+" ,"wynik":wynik, "title":"dodawanie"}
-	return render(
-    	    request=request,
-    	    template_name="maths/operation.html",
-    	    context=c
-	)
+    a, b = int(a), int(b)
+    wynik = a + b
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    result = Result.objects.get_or_create(value=wynik)[0]
+    Math.objects.create(operation='add', a=a, b=b, result=result)
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c
+    )
 
 def sub(request, a, b):
-   a, b = int(a), int(b)
-   wynik = a-b
-   c = {"a":a, "b":b, "operacja": "-" ,"wynik":wynik}
-   return render(
-    	    request=request,
-    	    template_name="maths/operation.html",
-    	    context=c
-	)
+    a, b = int(a), int(b)
+    wynik = a-b
+    c = {"a":a, "b":b, "operacja": "-" ,"wynik":wynik}
+    result = Result.objects.get_or_create(value=wynik)[0]
+    Math.objects.create(operation='sub', a=a, b=b, result=result)
+    return render(
+                request=request,
+                template_name="maths/operation.html",
+                context=c
+        )
 
 def mul(request, a, b):
-   a, b = int(a), int(b)
-   wynik = a*b
-   c = {"a":a, "b":b, "operacja": "*" ,"wynik":wynik}
-   return render(
-    	    request=request,
-    	    template_name="maths/operation.html",
-    	    context=c
-	)
+    a, b = int(a), int(b)
+    wynik = a*b
+    c = {"a":a, "b":b, "operacja": "*" ,"wynik":wynik}
+    result = Result.objects.get_or_create(value=wynik)[0]
+    Math.objects.create(operation='mul', a=a, b=b, result=result)
+    return render(
+                request=request,
+                template_name="maths/operation.html",
+                context=c
+        )
 
 def div(request, a, b):
-   if int(b) == 0:
-       wynik = "Error"
-       messages.add_message(request, messages.ERROR, "Dzielenie przez zero!")
-   else:
-       wynik = a / int(b)
-   c = {"a": a, "b": b, "operacja": "/", "wynik": wynik, "title": "dzielenie"}
-   return render(
-       request=request,
-       template_name="maths/operation.html",
-       context=c)
+    if int(b) == 0:
+        wynik = "Error"
+        messages.add_message(request, messages.ERROR, "Dzielenie przez zero!")
+    else:
+        wynik = a / int(b)
+    c = {"a": a, "b": b, "operacja": "/", "wynik": wynik, "title": "dzielenie"}
+    result = Result.objects.get_or_create(value=wynik)[0]
+    Math.objects.create(operation='div', a=a, b=b, result=result)
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c)
+
+def maths_list(request):
+    maths = Math.objects.all()
+    return render(
+        request=request,
+        template_name="maths/list.html",
+        context={"maths": maths}
+    )
+
+def math_details(request, id):
+    math = Math.objects.get(id=id)
+    return render(
+        request=request,
+        template_name="maths/details.html",
+        context={"math": math}
+    )
+
+def results_list(request):
+    if request.method == "POST":
+        form = ResultForm(data=request.POST)
+
+        if form.is_valid():
+            if form.cleaned_data['error'] == '':
+                form.cleaned_data['error'] = None
+            Result.objects.get_or_create(form.cleaned_data)
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Utworzono nowy Result!!"
+            )
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                form.errors['__all__']
+            )
+
+    form = ResultForm()
+    results = Result.objects.all()
+    return render(
+        request=request,
+        template_name="maths/results.html",
+        context={
+            "results": results,
+            "form": form
+        }
+    )
