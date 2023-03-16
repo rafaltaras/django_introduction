bea1e2485a83a0f699dfc089c3f24e6f7222139d
rafaltaras <paulo.dorosz@gmail.com>
2023-03-16 11:16:36 +0100

    db fixed

--- dingo/posts/models.py
+++ dingo/posts/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
    content = models.TextField()
    created = models.DateTimeField(auto_now_add=True)
    modified = models.DateTimeField(auto_now=True)
-   author_bio = models.TextField(null=True, blank=True)
    author = models.ForeignKey(
       'posts.Author',
       on_delete=models.CASCADE,
@@ -18,5 +17,6 @@ class Post(models.Model):
 class Author(models.Model):
     nick = models.CharField(max_length=5)
     email = models.EmailField(max_length=70,blank=True,unique=True)
+    author_bio = models.TextField(null=True, blank=True)
     def __str__(self):
         return f"id:{self.id}, nick={self.nick}, email={self.email}"
